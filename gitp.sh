4f1f8f01efa854def65acd8c537a37b124f5e688
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 16:42:30 -0400

    Improve error handling and branch creation for gitp.sh\n\nThis commit implements better error handling and more versatile branch creation for the gitp.sh script. Specifically, it adds a check to ensure that the correct number of arguments is passed when creating a new branch and allows for additional flags to be passed through when creating a new branch. Additionally, it refactors the "branch" option to improve readability and eliminate redundancy.

--- gitp.sh
+++ gitp.sh
@@ -135,17 +135,30 @@ if [ "$1" == "commit" ]; then
     rm "${tmp_file}"
 
 elif [ "$1" == "branch" ]; then
-    branches_output=$(git branch)
-    echo "${branches_output}" | while read -r branch_line; do
-        branch_name=$(echo "${branch_line}" | sed 's/^\* //;s/^  //')
-        branch_desc_ref="refs/notes/branch-descriptions/${branch_name}"
-        if git show-ref --quiet --verify "${branch_desc_ref}" 2>/dev/null; then
-            branch_desc=$(git notes --ref "branch-descriptions/${branch_name}" show | sed 's/^/  /')
-            echo -e "${branch_line}\n${branch_desc}\n"
-        else
-            echo "${branch_line}"
-        fi
-    done
+    shift
+
+    if [ "$#" -eq 0 ]; then
+        branches_output=$(git branch)
+        echo "${branches_output}" | while read -r branch_line; do
+            branch_name=$(echo "${branch_line}" | sed 's/^\* //;s/^  //')
+            branch_desc_ref="refs/notes/branch-descriptions/${branch_name}"
+            if git show-ref --quiet --verify "${branch_desc_ref}" 2>/dev/null; then
+                branch_desc=$(git notes --ref "branch-descriptions/${branch_name}" show | sed 's/^/  /')
+                echo -e "${branch_line}\n${branch_desc}\n"
+            else
+                echo "${branch_line}"
+            fi
+        done
+    else
+        passthrough_flags=()
+
+        while (( "$#" )); do
+            passthrough_flags+=( "$1" )
+            shift
+        done
+
+        git branch "${passthrough_flags[@]}"
+    fi
 elif [ "$1" == "checkout" ]; then
     shift
     intent=""
