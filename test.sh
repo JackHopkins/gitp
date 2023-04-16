144479850d0ffd6c21e8dfda5fa958cfc06dc172
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 16:00:18 -0400

    Subject: Add trimming of quotes around branch name returned from API
    Description: This change modifies the generate_branch_name function in `gitp.sh` to remove any quotation marks around the branch name returned from the API before returning it. This was necessary to ensure that the branch name is properly formatted and can be used in subsequent git commands. The commit message has been updated to reflect this change.

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-commit_message_full=$(echo "Subject\n\nBody" | tr -d '\r')
+response="Add trimming of quotes around branch name returned from API\n\nThis change modifies the generate_branch_name function in `gitp.sh` to remove any quotation marks around the branch name returned from the API before returning it. This was necessary to ensure that the branch name is properly formatted and can be used in subsequent git commands."
+
+commit_message_full=$(echo ${response} | tr -d '\r')
 commit_message_subject=$(echo "${commit_message_full}" | awk -F'\n\n' '{print $1}')
 commit_message_body=$(echo "${commit_message_full}" | awk -F'\n\n' '{print $2}' | sed 's/\\n/\n/g')
 
