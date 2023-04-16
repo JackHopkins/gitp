50ffac9fb3fcbb16854342d8a77fa922056d46bf
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 17:42:23 -0400

    Remove deleted files and modify generate_branch_name function
    Description: This commit removes the deleted files '.env' and 'test.sh' and modifies the 'generate_branch_name' function in 'gitp.sh'. The changes made ensure that any quotation marks around the branch name returned from the API are removed before returning it. This is necessary to properly format the branch name and make it usable in subsequent git commands. The commit message subject and body are generated using 'awk' and 'sed' commands to properly parse the response, and any carriage return characters are removed.

--- test.sh
+++
@@ -1,9 +0,0 @@
-response="Add trimming of quotes around branch name returned from API\n\nThis change modifies the generate_branch_name function in `gitp.sh` to remove any quotation marks around the branch name returned from the API before returning it. This was necessary to ensure that the branch name is properly formatted and can be used in subsequent git commands."
-
-commit_message_full=$(echo ${response} | tr -d '\r')
-commit_message_subject=$(echo "${commit_message_full}" | awk -F'\n\n' '{print $1}')
-commit_message_body=$(echo "${commit_message_full}" | awk -F'\n\n' '{print $2}' | sed 's/\\n/\n/g')
-
-echo "${commit_message_subject}"
-echo "Blah"
-echo "${commit_message_body}"
