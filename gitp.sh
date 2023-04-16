4f56583adb54290125f0f51a83cb22dcf6d0a487
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 14:39:33 -0400

    Add new function and argument handling to experimental branch

--- gitp.sh
+++ gitp.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+function generate_branch_name() {
+    local intent="$1"
+    local existing_branches="$2"
+    local gpt_message="Generate a new branch name based on the existing branches: ${existing_branches}. Intent: ${intent}."
+    gpt_message=$(echo "${gpt_message}" | tr -d '\n' | jq -sRr @json)
+    local payload="{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": ${gpt_message}}]}"
+
+    local api_response=$(curl -s -H "Content-Type: application/json" \
+                             -H "Authorization: Bearer ${GPT4_API_KEY}" \
+                             -d "${payload}" \
+                             https://api.openai.com/v1/chat/completions)
+
+    local error_message=$(echo "${api_response}" | jq -r '.error.message // empty')
+    if [ -n "${error_message}" ]; then
+        echo "An error occurred while generating the branch name:"
+        echo "${error_message}"
+        exit 1
+    fi
+
+    local branch_name=$(echo "${api_response}" | jq -r '.choices[0].message.content' | tr -d '\n')
+    echo "${branch_name}"
+}
+
 if [ -z "${GPT4_API_KEY}" ]; then
     if [ "$(uname)" == "Darwin" ]; then
         if [ "$SHELL" == "/bin/zsh" ]; then
@@ -85,7 +108,43 @@ if [ "$1" == "commit" ]; then
     else
         git commit -m "${commit_message}" "${passthrough_flags[@]}"
     fi
+elif [ "$1" == "checkout" ]; then
+    shift
+    intent=""
+    branch_flag=false
+    passthrough_flags=()
+
+    while (( "$#" )); do
+        case "$1" in
+            -i|--intent)
+                intent="$2"
+                shift 2
+                ;;
+            -b|--branch)
+                branch_flag=true
+                passthrough_flags+=( "$1" )
+                shift
+                ;;
+            *)
+                passthrough_flags+=( "$1" )
+                shift
+                ;;
+        esac
+    done
+
+    if [ -n "${intent}" ] && [ "${branch_flag}" = true ]; then
+        echo "Error: Cannot use both -i and -b flags simultaneously."
+        exit 1
+    fi
+
+    if [ -n "${intent}" ]; then
+        existing_branches=$(git branch --list | tr -d '* ' | jq -R . | jq -s .)
+        new_branch_name=$(generate_branch_name "${intent}" "${existing_branches}")
+        git checkout -b "${new_branch_name}"
+    else
+        git checkout "${passthrough_flags[@]}"
+    fi
+
 else
-    echo "Invalid command. Usage: gitp commit [-i intent] [-a] [other git commit flags]"
-    exit 1
+    git "$@"
 fi
