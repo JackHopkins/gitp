bccdd7e03f39332e20228267dcf868113fe4af6e
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 12:59:20 -0400

    null

--- gitp.sh
+++ gitp.sh
@@ -18,21 +18,39 @@ if [ "$1" == "commit" ]; then
     branch_name=$(git symbolic-ref --short -q HEAD)
     git_diff=$(git diff | jq -sRr @json)
 
-    if [ -z "${GPT4_API_KEY}" ]; then
-        read -p "Enter your GPT-4 API key: " GPT4_API_KEY
-        echo "export GPT4_API_KEY=${GPT4_API_KEY}" >> ~/.bashrc
-        source ~/.bashrc
+    if [ -z "${git_diff}" ] || [ "${git_diff}" == '""' ]; then
+        echo "No changes to commit. Exiting."
+        exit 0
     fi
 
-    echo ${GPT4_API_KEY}
-    echo ${git_diff}
+    if [ -z "${GPT4_API_KEY}" ]; then
+        read -p "Enter your GPT-4 API key: " GPT4_API_KEY
+        if [ "$(uname)" == "Darwin" ]; then
+            if [ "$SHELL" == "/bin/zsh" ]; then
+                echo "export GPT4_API_KEY=${GPT4_API_KEY}" >> ~/.zshrc
+                source ~/.zshrc
+            else
+                echo "export GPT4_API_KEY=${GPT4_API_KEY}" >> ~/.bash_profile
+                source ~/.bash_profile
+            fi
+        else
+            echo "export GPT4_API_KEY=${GPT4_API_KEY}" >> ~/.bashrc
+            source ~/.bashrc
+        fi
+    fi
+
+    # Prepare the GPT message content
+    gpt_message="Generate a commit message based on the following data: Branch: ${branch_name}. Git Diff: ${git_diff}."
+    if [ -n "${intent}" ]; then
+        gpt_message="${gpt_message} Intent: ${intent}."
+    fi
+
     # Pass the diff, branch name, and intent to GPT-3.5-turbo to generate the commit message
     commit_message=$(curl -s -H "Content-Type: application/json" \
                          -H "Authorization: Bearer ${GPT4_API_KEY}" \
-                         -d "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"Generate a commit message based on the following data: Branch: ${branch_name}. Diff: ${git_diff}. Intent: ${intent}.\"}]}" \
-                         https://api.openai.com/v1/chat/completions | jq -r '.choices[0].message.content' | tr -d '\n')
+                         -d "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"${gpt_message}\"}]}" \
+                         https://api.openai.com/v1/chat/completions | jq -r '.choices[0].message.text' | tr -d '\n')
 
-    echo ${commit_message}
     # Commit with the generated message
     git commit -m "${commit_message}"
 else
