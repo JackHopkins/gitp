a655ba872c534aebec0105101a75b1ee7557a946
Jack Hopkins <jack@raveler.co.uk>
2023-04-16 13:06:41 -0400

    null

--- gitp.sh
+++ gitp.sh
@@ -16,12 +16,15 @@ if [ "$1" == "commit" ]; then
     done
 
     branch_name=$(git symbolic-ref --short -q HEAD)
-    git_diff=$(git diff | jq -sRr @json)
+    git_diff=$(git diff --staged | jq -sRr @json)
 
     if [ -z "${git_diff}" ] || [ "${git_diff}" == '""' ]; then
         echo "No changes to commit. Exiting."
         exit 0
     fi
+
+    echo ${git_diff}
+    echo ${branch_name}
 
     if [ -z "${GPT4_API_KEY}" ]; then
         read -p "Enter your GPT-4 API key: " GPT4_API_KEY
@@ -45,10 +48,12 @@ if [ "$1" == "commit" ]; then
         gpt_message="${gpt_message} Intent: ${intent}."
     fi
 
+    payload="{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"${gpt_message}\"}]}"
+    echo ${payload}
     # Pass the diff, branch name, and intent to GPT-3.5-turbo to generate the commit message
     commit_message=$(curl -s -H "Content-Type: application/json" \
                          -H "Authorization: Bearer ${GPT4_API_KEY}" \
-                         -d "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"${gpt_message}\"}]}" \
+                         -d "${payload}" \
                          https://api.openai.com/v1/chat/completions | jq -r '.choices[0].message.text' | tr -d '\n')
 
     # Commit with the generated message
