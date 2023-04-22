7942f7280e7b6673817d1b70b72a324dbe9ad28b
Jack Hopkins <jack@raveler.co.uk>
2023-04-21 20:29:43 -0400

    Add generate_branch_name and git diff analysis
    This commit adds a new function called generate_branch_name to the test.sh file. It also adds a section to analyze the git diff output in the uctags.sh file. This analysis saves the git diff output to a temporary file, reads it, extracts the list of modified functions and variables using perl, removes the '+' and '()' characters to get the item names, and then searches for the definitions of those items in the tags file. Additionally, the global command in uctags.sh is now called with the '-u' option instead of no option, and some minor changes were made to the command used to extract the list of modified functions and variables.

---
+++ test.sh
@@ -0,0 +1,1 @@
+generate_branch_name()
--- uctags.sh
+++ uctags.sh
@@ -20,6 +20,36 @@ else
     exit 1
 fi
 
+ctags -R --fields=+l --languages=-all --languages=+sh,+c,+C++,+python --extras=+q --exclude=.git --exclude=.gitp -f .gitp/tags
+
+# Save git diff output to a temporary file
+temp_diff_file=$(mktemp)
+git diff HEAD~1 > "$temp_diff_file"
+
+# Read git diff output from the temporary file
+git_diff=$(cat "$temp_diff_file")
+echo "$temp_diff_file"
+#echo "$git_diff"
+# Get the list of modified functions and variables from the diff
+#modified_items=$(echo "$git_diff" | grep -oP '^\+[\w_]+\(\)')
+
+# Get the list of modified functions and variables from the diff using perl
+modified_items=$(echo "$git_diff" | perl -nle 'print $& if m{^\+[\w_]+\(\)}')
+
+
+echo $modified_items
+# Remove the '+' and '()' characters to get the item names
+modified_item_names=$(echo "$modified_items" | sed 's/+\(.*\)(.*/\1/')
+
+# Iterate through the modified item names and find their definitions in the tags file
+while read -r item_name; do
+  if [[ ! -z "$item_name" ]]; then
+    tag_line=$(grep -m 1 "^$item_name" tags)
+    echo "$tag_line"
+  fi
+done <<< "$modified_item_names"
+
+exit 1
 # Set the GTAGSROOT environment variable to point to the .gitp directory
 export GTAGSROOT=$(pwd)/.gitp
 
@@ -38,9 +68,10 @@ echo "---"
 echo "$all_files"
 
 # Get the tags from the global command
-tags=$(global .)
+tags=$(global -u)
 echo "$tags"
 
+exit 1
 # Get the most recent changes
 echo "Getting the most recent changes with 'git diff'..."
 git_diff_output=$(git diff HEAD~1 --name-status)
@@ -56,13 +87,12 @@ changed_methods_and_members=""
 for file in $changed_files; do
     git_diff_methods_and_members=$(git diff HEAD~1 -U0 -- "$file" | \
         awk '/^[\+\-]((public|private|protected|static)[[:space:]]+)*[a-zA-Z0-9_]+[[:space:]]+[a-zA-Z0-9_]+[[:space:]]*\(/ { gsub(/^[\+\-]/, "", $0); print $0} /^[\+\-](function[[:space:]]+)?[a-zA-Z0-9_]+[[:space:]]*\(\)/ { gsub(/^[\+\-]/, "", $0); print $0} /^[\+\-][a-zA-Z0-9_]+=/ { gsub(/^[\+\-]/, "", $0); print $0}' | \
-        sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
+        { git_diff_methods_and_members=""; first_line=1; while IFS= read -r line || [[ -n "$line" ]]; do if [ "$first_line" -eq 1 ]; then first_line=0; else git_diff_methods_and_members+="\\n"; fi; git_diff_methods_and_members+="$line"; done; echo -n "$git_diff_methods_and_members"; })
     changed_methods_and_members+="$git_diff_methods_and_members"$'\n'
 done
 
 echo "Changed methods and members:"
 echo "$changed_methods_and_members"
-exit 1
 
 # Ensure GNU Global tag files are updated
 #gtags --gtagslabel=pygments -f .gitp
@@ -75,9 +105,9 @@ for method_or_member in $changed_methods_and_members; do
 
     # Find references
     echo "References of '$clean_method_or_member':"
-    global -f -r -x "$clean_method_or_member"
+    global -xg "$clean_method_or_member"
 
     # Find declarations
     echo "Declarations of '$clean_method_or_member':"
-    global -f -d -x "$clean_method_or_member"
+    global -xg "$clean_method_or_member"
 done
